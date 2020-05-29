a2aefa5349ef93a8b7482d1bbf21dc5bdddfd127
Highom <highomlp@gmail.com>
2020-05-29 10:00:31 +0200

    Ported to react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import Cat from './Cat.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="jumbotron text-center">
+        <h1>Welcome to Highom's Space</h1>
+        <p>This Site is still in development, check back later</p>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-xl-4 col-sm-8 col-10">
+          <img src={Cat} class="img-fluid rounded" alt="Gif of a Cat sleeping" />
+      </div>
+      </div>
     </div>
   );
 }
