0559101e1efbccde062f68599fafc13020a54986
Highom <highomlp@gmail.com>
2020-05-29 10:05:52 +0200

    Updates

---
+++ precache-manifest.88e1e6d436ba7230519e309d0248cf92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "065db25a38252c06d340b91e74e12ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "940f4a49b46d6d5e43ed",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "350656888cf4c6d24c8a",
+    "url": "/static/js/2.eddff1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eddff1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "940f4a49b46d6d5e43ed",
+    "url": "/static/js/main.2b116045.chunk.js"
+  },
+  {
+    "revision": "d3cb7b31c4b37c701b70",
+    "url": "/static/js/runtime-main.fc392b72.js"
+  },
+  {
+    "revision": "81d72fa40f2bbae5528b483431b4feea",
+    "url": "/static/media/Cat.81d72fa4.gif"
+  }
+]);
